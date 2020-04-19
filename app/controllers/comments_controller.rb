3421ef054c3debd23e8d71e7d28ef4377f9da820
Gareth Harris <gdharris1000@gmail.com>
2020-04-19 13:32:56 +0100

    edit and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :find_comment, only: [:edit, :update, :destroy]
+
   def index
   end
 
@@ -16,11 +18,27 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    @comment.update(comment_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to posts_path
+  end
+
   private
+
+  def find_comment
+    @comment = Comment.find(params[:id])  
+  end
 
   def comment_params
     params.require(:comment).permit(:comment_text, :user_id, :post_id)
   end
-
 
 end
