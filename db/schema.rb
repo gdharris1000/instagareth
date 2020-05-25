be349d7797858728a8dd452481fe213b9a631a6c
Gareth Harris <gdharris1000@gmail.com>
2020-05-25 10:45:28 +0100

    made comment info smaller, added google font

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_185809) do
+ActiveRecord::Schema.define(version: 2020_04_18_175332) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
